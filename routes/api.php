6e52e1d61826036d0481f4f9f971c20d2f95ec92
ahmedmohamedeid98 <ahmedmohamedeid98@gmail.com>
2022-02-03 18:30:43 +0200

    auth: add ApiAuthController

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\ApiAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', [ApiAuthController::class, 'login'])->middleware(['cors', 'json.reponse']);
+Route::post('/register', [ApiAuthController::class, 'register'])->middleware(['cors', 'json.reponse']);
+Route::post('/logout', [ApiAuthController::class, 'register'])->middleware('auth:api');
