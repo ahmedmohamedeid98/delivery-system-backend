a4c922214049e2efc57ed8f0f0564e9b995b198b
ahmedmohamedeid98 <ahmedmohamedeid98@gmail.com>
2022-02-06 07:31:26 +0200

    auth: reset password configuration

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,16 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
+use App\Notifications\ResetPasswordNotification;
 
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, CanResetPassword;
 
     /**
      * The attributes that are mass assignable.
@@ -22,6 +24,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'google_id',
+        'facebook_id',
+        'photo_url'
     ];
 
     /**
@@ -42,4 +47,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function sendPasswordResetNotification($token) {
+        $url = "http://localhost:4200/reset-password?token=".$token;
+        $this->notify(new ResetPasswordNotification($url));
+    }
 }
