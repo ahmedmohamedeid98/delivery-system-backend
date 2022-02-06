27a386715f908b1e000097505f76617f7d2894e1
ahmedmohamedeid98 <ahmedmohamedeid98@gmail.com>
2022-02-06 07:30:23 +0200

    auth: add userResource class

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->getEmail(),
+            'photo_url' => $this->photo_url,
+            'created_at' => $this->created_at
+        ];
+    }
+
+    private function getEmail()
+    {
+        // check email 
+        if($this->facebook_id == null) {
+            return $this->email;
+        } else {
+            $username = explode('@', $this->email)[0];
+            if($username == $this->facebook_id) {
+                return null;
+            } else {
+                return $this->email;
+            }
+        }
+    }
+}
