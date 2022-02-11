b589fbb88d1294de9e937d40fed0c80cb21a8ad3
ahmedmohamedeid98 <ahmedmohamedeid98@gmail.com>
2022-02-12 01:51:52 +0200

    setup paytabs payment gateway configuration

--- config/logging.php
+++ config/logging.php
@@ -113,6 +113,12 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+
+        'PayTabs' => [  
+            'driver' => 'single',  
+            'path' => storage_path('logs/paytabs.log'),  
+            'level' => 'info',  
+            ],
     ],
 
 ];
