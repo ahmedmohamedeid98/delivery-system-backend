79585124808ca61bc6c3ef5efd2875e9037ef6c7
ahmedmohamedeid98 <ahmedmohamedeid98@gmail.com>
2022-02-14 05:53:45 +0200

    set default value for order cost

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ApplyTaskRequest;
 use App\Http\Requests\CreateTaskRequest;
+use App\Http\Resources\TaskOfferResource;
 use App\Http\Resources\TaskResource;
+use App\Http\Resources\UserResource;
 use App\Models\Task;
+use App\Models\User;
+use App\Models\UserRequestTask;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -30,7 +35,7 @@ class TaskController extends Controller
                 'travel_status' => 0, // by default not-start
                 'description' => $task['description'],
                 'budget' => $task['budget'],
-                'order_cost' => $task['order_cost'],
+                'order_cost' => isset($task['order_cost']) ? $task['order_cost'] : 0,
                 'payment_method' => $task['payment_method'],
                 'required_invoice' => $task['required_invoice'],
                 'note' => isset($task['note']) ? $task['note'] : '',
