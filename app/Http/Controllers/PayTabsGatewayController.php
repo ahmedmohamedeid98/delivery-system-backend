7f776ef3dedab10a535430815b81cd116ca7d652
ahmedmohamedeid98 <ahmedmohamedeid98@gmail.com>
2022-02-26 23:49:33 +0200

    update spent_amount when user pay for task

--- app/Http/Controllers/PayTabsGatewayController.php
+++ app/Http/Controllers/PayTabsGatewayController.php
@@ -135,6 +135,10 @@ class PayTabsGatewayController extends Controller
         } else if ($trans_type == "both") {
           $task->paid_both = $trans_amount;
         }
+        $profile = Profile::where('user_id', $user_id);
+        $spent_amount = $profile->spent_amount;
+        $profile->spent_amount = $spent_amount + $trans_amount;
+        $profile->save();
         $task->save();
       }
     } catch (Exception $e) {
