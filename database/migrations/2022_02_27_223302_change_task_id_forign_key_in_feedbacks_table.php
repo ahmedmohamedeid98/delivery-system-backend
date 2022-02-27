c8c7292897ab3cff69c3f21a24c4e2805a557d92
ahmedmohamedeid98 <ahmedmohamedeid98@gmail.com>
2022-02-28 00:38:23 +0200

    convert task forign key to cascade on delete

---
+++ database/migrations/2022_02_27_223302_change_task_id_forign_key_in_feedbacks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeTaskIdForignKeyInFeedbacksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('feedback', function (Blueprint $table) {
+            $table->dropForeign('feedback_task_id_foreign');
+            $table->foreign('task_id')->references('id')->on('tasks')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('feedbacks', function (Blueprint $table) {
+            //
+        });
+    }
+}
